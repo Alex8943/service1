6566f8cae29101bf6559fa3a64f1137a1d40ba92
Alexander <65241585+Alex8943@users.noreply.github.com>
2024-12-03 11:45:34 +0100

    other services setup

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,15 @@
 import express from 'express';
 import { testDBConnection } from './db_service/db_connection';
+import logger from './other_services/winstonLogger';
 const app = express();
 
 testDBConnection();
+
+process.on('SIGINT', () => {
+    logger.end();
+    console.log('See ya later silly');
+    process.exit(0);
+  });
 
 app.listen(3001, () => {
     console.log("Server1 is running on port 3001");
