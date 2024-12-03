c1579617befb90a327b46a4d40cb4722f6be212a
Alexander <65241585+Alex8943@users.noreply.github.com>
2024-12-03 11:33:06 +0100

    database cconnection established

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,8 @@
 import express from 'express';
+import { testDBConnection } from './db_service/db_connection';
 const app = express();
 
-
+testDBConnection();
 
 app.listen(3001, () => {
     console.log("Server1 is running on port 3001");
